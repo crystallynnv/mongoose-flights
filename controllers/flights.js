203ae4ba57e488706ac94ce0eee371c5bb19f0d4
Home <home@homes-mbp.lan>
2020-02-25 21:20:31 -0600

    Add details link

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,12 +1,34 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-    index
+    index,
+    new: newFlight,
+    create,
+    show
 }
 
-function index(req, res) {
+function show(req, res) {
+    Flight.findById(req.params,id, function(err, flight){
+        res.render('flights/index')
+    });
+}
+
+function index(req, res){
     Flight.find({}, function(err, flights){
-        if (err) return next();
+        if (err) return next(err);
         res.render('flights/index', {flights})
+  });
+}
+
+function newFlight(req, res){
+    res.render('flights/new')
+}
+
+function create(req, res){
+    console.log(req.body);
+    const flight = new Flight(req.body);
+    flight.save(function(err){
+        if (err) return res.render('flights/new')
+        res.redirect('/flights');
     })
 }
--- models/flight.js
+++ models/flight.js
@@ -2,20 +2,41 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+  airport: {
+  type: String,
+  enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  }, 
+  arrival: {
+  type: Date
+  }
+});
+
 const flightSchema = new Schema({
     airline: {
         type: String,
-        enum: ['American', 'United', 'Southwest']
+        enum:['American', 'Southwest', 'United']
     },
-    flightNum: {
-        type: Number,
-        min: 10,
-        max: 9999,
-        required: true
-    },
-    departs: {
-        type: Date,
-        default: function() {
-            return new Date().getFullYear() + 1;
-        }
-}})
+    flightNo: {
+      type: Number,
+      min: 10,
+      max: 9999,
+      required: true
+  },
+  departs: {
+      type: Date,
+      default: function() {
+          return new Date().getFullYear() + 1;
+      }
+  },
+  airport: {
+      type: String,
+      enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+      default: 'DEN'
+  },
+  destinations: {
+      type: [destinationSchema]
+  }
+})
+
+module.exports = mongoose.model('Flight', flightSchema);
